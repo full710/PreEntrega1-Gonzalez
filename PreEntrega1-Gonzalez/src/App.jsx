ed721f45da1a2857ccaa3eecafc1f68ddebbe727
Juan Gonzalez <juanig.gonzalez.7@gmail.com>
2024-09-24 21:35:32 -0300

    Last commit PreEntrega1

--- PreEntrega1-Gonzalez/src/App.jsx
+++ PreEntrega1-Gonzalez/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
-
+import './global/styles.css'
 
 function App() {
   const [count, setCount] = useState(0)
